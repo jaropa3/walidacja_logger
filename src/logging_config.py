6d57addcd2973fbc4af09d33519b787dbbe0c3db
jaropa3 <jarek.opasinksi@gmail.com>
2026-02-23 19:00:16 +0100

    dodanie formatu czasu

--- src/logging_config.py
+++ src/logging_config.py
@@ -5,4 +5,5 @@ def setup_logging():
         filename="app.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
